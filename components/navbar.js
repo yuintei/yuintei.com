81e88d2ad100b42af04bc73cf5cb723aa066c794
yuintei <yuin0106@gmail.com>
2022-09-06 21:03:25 +0900

    add blur backdrop

--- components/navbar.js
+++ components/navbar.js
@@ -15,7 +15,7 @@ export default function Navbar() {
     <Disclosure as="nav" className=" sticky top-0">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-4xl px-2 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-4xl px-2 sm:px-6 lg:px-8 backdrop-blur-lg">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
