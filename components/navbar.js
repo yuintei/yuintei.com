6fec7139f53e4297ad274d3fd0e6d01fd04fff5f
yuintei <yuin0106@gmail.com>
2022-09-06 20:55:12 +0900

    change navbar bg-color

--- components/navbar.js
+++ components/navbar.js
@@ -12,14 +12,14 @@ const navigation = [
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className=" sticky top-0 bg-gray-800">
+    <Disclosure as="nav" className=" sticky top-0">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-4xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-500 dark:text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -45,7 +45,7 @@ export default function Navbar() {
                     {navigation.map((item) => (
                       <Link href={item.href} key={item.name}>
                         <a
-                          className="text-gray-300 hover:bg-gray-700 hover:text-white
+                          className="text-gray-500 dark:text-gray-400 hover:bg-gray-700 hover:text-white
                             px-3 py-2 rounded-md text-sm font-medium"
                           aria-current={item.current ? "page" : undefined}
                         >
@@ -67,7 +67,7 @@ export default function Navbar() {
                   key={item.name}
                   as="a"
                   href={item.href}
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white
+                  className="text-gray-500 dark:text-gray-400 hover:bg-gray-700 hover:text-white
                     block px-3 py-2 rounded-md text-base font-medium"
                   aria-current={item.current ? "page" : undefined}
                 >
